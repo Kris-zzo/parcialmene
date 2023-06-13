4648a732f9dc9c30edf563307f17718c5dd224ad
kristel <kristel1menegue89@gmail.com>
2023-06-13 10:47:53 -0600

    se agregaron temas

---
+++ Documents/parcialk/temas.py
@@ -0,0 +1,16 @@
+# Temas que no se me hacian dificiles pero no entendia 
+
+tema1 = {"Tema 1": "Normalizacion", "Motivo" : "Me costaba ya que me confundia ver tantos datos y saber cual iba en la 1fn, 2fn y 3fn"}
+print(tema1 , end="\n\n")
+
+tema2 = {"Tema 2": "Python", "Motivo": "Me costaba ya que aveces pues se me olvidan cosas y no recuerdo como hacerlas"}
+print(tema2 , end="\n\n") 
+
+tema3 = {"Tema 3": "Gitbash" , "Motivo" : "Me costaba por que habian cosas que se me confundian con otras"}
+print(tema3 , end="\n\n")
+
+
+
+
+
+
