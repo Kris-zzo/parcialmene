55c21677fc8ec57e150407fdb8d17b1e74fdc6e1
kristel <kristel1menegue89@gmail.com>
2023-06-15 10:30:25 -0600

    se agrego operacion residuo

--- Desktop/laboratorio2k/Operaciones.py
+++ Desktop/laboratorio2k/Operaciones.py
@@ -1,14 +1,64 @@
 # operaciones dadas en clase
 
+# suma
+
 def suma (a,b):
     '''suma dos numeros'''
     resultado= a + b 
     return resultado
 
+# resta
+
+def resta (a,b):
+    '''resta dos numeros'''
+    return a - b
+
+# Multiplicacion 
+
+def multiplicacion (a,b):
+    '''multiplica dos numeros'''
+    return a * b
+
+# Division
+
+def Division (a,b):
+    '''Divide dos numeros'''
+    return a / b
+
+# Exponenciacion
+
+def Exponenciacion (a,b):
+    '''Eleva un numero '''
+    return a ** b 
+
+# raices
+
+def raiz (a):
+    '''raiz de un numeros'''
+    import math
+    return  math.sqrt(a)
+ 
+# suma5
+
+def suma5 ():
+    '''suma 5 numeros'''
+    suma=1
+    for i in range (1,6):
+        s= int (input(f"ingrese {i} numero: "))
+        suma = suma + s
+    print(suma)
+      
+suma5()
+
+# residuo
+
+def Residuo (a,b):
+    '''porcentaje de un numero '''
+    return a % b 
 
 
+def run ():
+    suma5 ()
 
-
-
-
-
+if __name__ == '_main_':
+        run ()
