c91ad38e71299761b07ad7fd4f04cc82eda837ef
kristel <kristel1menegue89@gmail.com>
2023-06-15 08:38:46 -0600

    se agrego operacion resta

--- Desktop/laboratorio2k/Operaciones.py
+++ Desktop/laboratorio2k/Operaciones.py
@@ -1,10 +1,17 @@
 # operaciones dadas en clase
 
+# suma
+
 def suma (a,b):
     '''suma dos numeros'''
     resultado= a + b 
     return resultado
 
+# resta
+
+def resta (a,b):
+    '''resta dos numeros'''
+    return a - b
 
 
 
