b8b307b698368d093e8d09be4b9aac2ea62c35a8
kristel <kristel1menegue89@gmail.com>
2023-06-15 08:48:24 -0600

    se agrego operacion Division

--- Desktop/laboratorio2k/Operaciones.py
+++ Desktop/laboratorio2k/Operaciones.py
@@ -1,11 +1,29 @@
 # operaciones dadas en clase
 
+# suma
+
 def suma (a,b):
     '''suma dos numeros'''
     resultado= a + b 
     return resultado
 
+# resta
 
+def resta (a,b):
+    '''resta dos numeros'''
+    return a - b
+
+# Multiplicacion 
+
+def multiplicacion (a,b):
+    '''multiplica dos numeros'''
+    return a * b
+
+# Division
+
+def Division (a,b):
+    '''Divide dos numeros'''
+    return a / b
 
 
 
