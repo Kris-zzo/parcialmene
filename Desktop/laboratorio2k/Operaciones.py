87b534f0fad696e3d9c6474a583ba8ff3532c394
kristel <kristel1menegue89@gmail.com>
2023-06-15 08:45:35 -0600

    se agrego operacion multiplicacion

--- Desktop/laboratorio2k/Operaciones.py
+++ Desktop/laboratorio2k/Operaciones.py
@@ -1,11 +1,23 @@
 # operaciones dadas en clase
 
+# suma
+
 def suma (a,b):
     '''suma dos numeros'''
     resultado= a + b 
     return resultado
 
+# resta
 
+def resta (a,b):
+    '''resta dos numeros'''
+    return a - b
+
+# Multiplicacion 
+
+def multiplicacion (a,b):
+    '''multiplica dos numeros'''
+    return a * b
 
 
 
