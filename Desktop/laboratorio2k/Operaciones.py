7ab7ab2902e9cedac64120bcca40ef3419d26421
kristel <kristel1menegue89@gmail.com>
2023-06-15 10:02:48 -0600

    se agrego operacion raices

--- Desktop/laboratorio2k/Operaciones.py
+++ Desktop/laboratorio2k/Operaciones.py
@@ -1,14 +1,49 @@
 # operaciones dadas en clase
 
+# suma
+
 def suma (a,b):
     '''suma dos numeros'''
     resultado= a + b 
     return resultado
 
+# resta
 
+def resta (a,b):
+    '''resta dos numeros'''
+    return a - b
 
+# Multiplicacion 
 
+def multiplicacion (a,b):
+    '''multiplica dos numeros'''
+    return a * b
 
+# Division
 
+def Division (a,b):
+    '''Divide dos numeros'''
+    return a / b
+
+# Exponenciacion
+
+def Exponenciacion (a,b):
+    '''Eleva un numero '''
+    return a ** b 
+
+# raices
+
+def raiz (a):
+    '''raiz de un numeros'''
+    import math
+    return  math.sqrt(a)
+ 
+ 
+
+def run ():
+    pass
+
+if __name__ == '_main_':
+        run ()
 
 
---
+++ Desktop/laboratorio2k/main.py
@@ -0,0 +1,7 @@
+from Operaciones import suma,resta,multiplicacion,Division,Exponenciacion,raiz
+
+total_suma= suma (10,10)
+print (total_suma)
+
+total_raiz= raiz (2)
+print (total_raiz)
